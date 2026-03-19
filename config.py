cf6f076c13486c1b582d46b5eb277b9a3fb6ac80
ynchhk <ankravcena@gmail.com>
2026-03-19 22:56:06 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py
 # Вставь сюда токен бота от @BotFather
-BOT_TOKEN = "ВАШ_ТОКЕН_ЗДЕСЬ"
+BOT_TOKEN = "8697824599:AAFs-XU5bL2HCeZr7pLSoxxKVoDuYHuxclI"
 
 # ID группы в системе РУЗ СПбПУ (5130903/50002 = 42673)
 GROUP_ID = 42673
